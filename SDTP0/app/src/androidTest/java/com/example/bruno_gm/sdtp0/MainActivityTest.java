e0558ec3ad0f7a1da11d7b4ecfe6729c47ac4b4c
Bruno Goulart Meneghin <ero-xd@hotmail.com>
2017-03-22 23:42:28 -0300

    foi

---
+++ SDTP0/app/src/androidTest/java/com/example/bruno_gm/sdtp0/MainActivityTest.java
@@ -0,0 +1,15 @@
+package com.example.bruno_gm.sdtp0;
+
+import org.junit.Test;
+
+/**
+ * Created by Bruno-GM on 22/03/2017.
+ */
+
+public class MainActivityTest {
+    @Test
+    public void onCreate() throws Exception {
+
+    }
+
+}
